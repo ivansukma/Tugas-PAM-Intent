e40dc917de97a75fa4bc92423b3f40a2f700393c
robertusbagaskara <robertusbagaskara@gmail.com>
2021-02-06 03:05:32 +0700

    minor update

--- bmicalculator/app/src/main/java/com/ftiuksw/bmicalculator/CalculatorActivity.java
+++ bmicalculator/app/src/main/java/com/ftiuksw/bmicalculator/CalculatorActivity.java
@@ -2,11 +2,13 @@ package com.ftiuksw.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,14 +35,22 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         btnHitung.setOnClickListener(this);
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_hitung) {
             String inputBerat = edtBerat.getText().toString().trim();
             String inputTinggi = edtTinggi.getText().toString().trim();
-
             double imb = Double.valueOf(inputBerat) / (Double.valueOf(inputTinggi) * Double.valueOf(inputTinggi) / 10000);
-            tvHasil.setText(String.valueOf(imb));
+            String kategori = "IDEAL";
+            if (imb >= 25.1) {
+                kategori = "OVERWEIGHT";
+            } else if (imb < 18.5) {
+                kategori = "UNDERWEIGHT";
+            }
+
+            String result_message = String.format("%.2f", imb) + " (" + kategori + ")";
+            tvHasil.setText(result_message);
         }
     }
 }
