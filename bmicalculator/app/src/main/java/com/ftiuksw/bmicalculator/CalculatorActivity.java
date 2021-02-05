2a1c261c7f6e4e69c2e6cac786e6c779a4851107
robertusbagaskara <robertusbagaskara@gmail.com>
2021-02-05 09:02:07 +0700

    add calculator activity

---
+++ bmicalculator/app/src/main/java/com/ftiuksw/bmicalculator/CalculatorActivity.java
@@ -0,0 +1,41 @@
+package com.ftiuksw.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtBerat;
+    private EditText edtTinggi;
+    private Button btnHitung;
+    private TextView tvHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        edtBerat = findViewById(R.id.edt_berat);
+        edtTinggi = findViewById(R.id.edt_tinggi);
+        btnHitung = findViewById(R.id.btn_hitung);
+        tvHasil = findViewById(R.id.tv_hasil);
+
+        btnHitung.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_hitung) {
+            String inputBerat = edtBerat.getText().toString().trim();
+            String inputTinggi = edtTinggi.getText().toString().trim();
+
+            double imb = Double.valueOf(inputBerat) / (Double.valueOf(inputTinggi) * Double.valueOf(inputTinggi) / 10000);
+            tvHasil.setText(String.valueOf(imb));
+        }
+    }
+}
